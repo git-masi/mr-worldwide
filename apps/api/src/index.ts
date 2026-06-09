203aa38726bb8aaf2c5d507e7f601a4734def858
Eric Masi <eric.masi.dev@gmail.com>
2026-06-09 19:53:58 -0400

    Serialize bigint values

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -6,6 +6,13 @@ if (!dsn) {
   throw new Error("Missing env var DATABASE_URL");
 }
 
+// This allows `BigInt` values to be serialized using `JSON.stringify`.
+// There are alternative ways to achieve the same results, but this is the easiest for our use case.
+// @ts-ignore
+BigInt.prototype.toJSON = function () {
+  return this.toString();
+};
+
 const server = initServer(dsn);
 
 const port = process.env.PORT ?? 1337;
