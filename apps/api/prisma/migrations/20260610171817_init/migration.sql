5871e3b9246e3ac057fd411a77a0ec5cb098c418
Eric Masi <eric.masi.dev@gmail.com>
2026-06-10 13:18:40 -0400

    Add migration

---
+++ apps/api/prisma/migrations/20260610171817_init/migration.sql
@@ -0,0 +1,36 @@
+-- CreateTable
+CREATE TABLE "hotels" (
+    "id" BIGSERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "totalRooms" INTEGER NOT NULL,
+
+    CONSTRAINT "hotels_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "bookings" (
+    "id" BIGSERIAL NOT NULL,
+    "hotel_id" BIGINT NOT NULL,
+    "customer_id" BIGINT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "check_in" TIMESTAMPTZ NOT NULL,
+    "check_out" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "bookings_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "customers" (
+    "id" BIGSERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "customers_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "bookings" ADD CONSTRAINT "bookings_hotel_id_fkey" FOREIGN KEY ("hotel_id") REFERENCES "hotels"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bookings" ADD CONSTRAINT "bookings_customer_id_fkey" FOREIGN KEY ("customer_id") REFERENCES "customers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
