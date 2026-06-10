b201d02f6e9f07699cfc1c36bddaab78e6b0248a
Eric Masi <eric.masi.dev@gmail.com>
2026-06-10 11:31:15 -0400

    Add test

--- apps/api/prisma/__tests__/rooms.test.ts
+++ apps/api/prisma/__tests__/rooms.test.ts
@@ -9,4 +9,11 @@ describe("Rooms", () => {
       expect(() => new Rooms(numRooms)).toThrow("Must have 1 or more rooms");
     });
   });
+
+  test("return available rooms", () => {
+    const totalRooms = 10;
+    const rooms = new Rooms(totalRooms);
+
+    expect(rooms.getNumAvailableRooms()).toEqual(totalRooms);
+  });
 });
