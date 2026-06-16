e55eaa38f3ba0c430cec7d35c51f924152910978
Eric Masi <eric.masi.dev@gmail.com>
2026-06-16 11:16:42 -0400

    Edit comment

--- packages/numbers/src/range.ts
+++ packages/numbers/src/range.ts
@@ -1,6 +1,6 @@
-/*
-  Create an iterator that can be used to range from 0 to n - 1
-*/
+/**
+ * Create an iterator that can be used to range from 0 to n - 1
+ */
 export function* range(n: number) {
   for (let i = 0; i < n; i++) {
     yield i;
