c47e88bfc043508fa3d21faec0653a9c4bcea4e5
Eric Masi <eric.masi.dev@gmail.com>
2026-06-29 15:53:39 -0400

    Add test

--- apps/api/src/__tests__/server.spec.ts
+++ apps/api/src/__tests__/server.spec.ts
@@ -155,6 +155,98 @@ describe("server integration tests", () => {
           }),
         ]);
       });
+
+      test("return results for multiple hotels", async () => {
+        const hotel1Name = faker.company.name();
+        const hotel2Name = faker.company.name();
+        const hotel3Name = faker.company.name();
+        const checkIn = "2026-01-01";
+        const checkOut = "2026-01-05";
+
+        const [{ id: hotel1Id }, { id: hotel2Id }, { id: hotel3Id }] =
+          await prisma.hotel.createManyAndReturn({
+            data: [
+              {
+                name: hotel1Name,
+                totalRooms: 2,
+              },
+              {
+                name: hotel2Name,
+                totalRooms: 2,
+              },
+              {
+                name: hotel3Name,
+                totalRooms: 10,
+              },
+            ],
+            select: { id: true },
+          });
+
+        const { id: guestId } = await prisma.guest.create({
+          data: {
+            firstName: faker.person.firstName(),
+            lastName: faker.person.lastName(),
+            email: faker.internet.email(),
+          },
+          select: { id: true },
+        });
+
+        const baseBooking = {
+          guestId,
+          checkIn: new Date(checkIn),
+          checkOut: new Date(checkOut),
+        };
+
+        await prisma.booking.createMany({
+          data: [
+            // 1 booking for hotel 1
+            {
+              ...baseBooking,
+              hotelId: hotel1Id,
+            },
+            // 2 bookings for hotel 2
+            {
+              ...baseBooking,
+              hotelId: hotel2Id,
+            },
+            {
+              ...baseBooking,
+              hotelId: hotel2Id,
+            },
+            // 1 booking for hotel 3
+            {
+              ...baseBooking,
+              hotelId: hotel3Id,
+            },
+          ],
+        });
+
+        const res = await request(server)
+          .get(path)
+          .query({ checkIn: checkIn, checkOut: checkOut });
+
+        expect(res.status).toEqual(200);
+
+        expect(res.body).toMatchObject(
+          expect.arrayContaining([
+            expect.objectContaining({
+              name: hotel1Name,
+              id: hotel1Id.toString(),
+              availableRooms: 1,
+            }),
+            expect.objectContaining({
+              name: hotel2Name,
+              id: hotel2Id.toString(),
+              availableRooms: 0,
+            }),
+            expect.objectContaining({
+              name: hotel3Name,
+              id: hotel3Id.toString(),
+              availableRooms: 9,
+            }),
+          ]),
+        );
+      });
     });
   });
 });
